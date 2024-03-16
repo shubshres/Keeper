d2b764945a845c25ed26d579cf755158d817a786
Shubhayu Shrestha <shresthashubhayu@gmail.com>
2024-03-16 11:58:53 -0500

    initial build

---
+++ components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const headerName = "Keeper"
+
+function Header(){
+    return (
+        <header>
+            <h1>{headerName}</h1>
+        </header>
+    )
+}
+
+export default Header; 
